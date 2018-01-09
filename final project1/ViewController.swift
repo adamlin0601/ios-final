066411e2c219ec85d521e7a9c7a51ceaf184fc06
adam lin <adamlin@adamde-Mac.local>
2018-01-09 01:47:37 -0800

    Initial Commit

---
+++ final project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  final project1
+//
+//  Created by adam lin on 2018/1/9.
+//  Copyright © 2018年 adam lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
